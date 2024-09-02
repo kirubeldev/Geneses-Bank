4d6eb156c5242b5e95d2a47469239bcf8859dde6
=Kirubel Meneberu <=kirubel.menberu@kegeberewtech.com>
2024-09-02 14:58:13 +0300

    fixeeed

--- app/componet/FAQ.jsx
+++ app/componet/FAQ.jsx
@@ -2,7 +2,7 @@
 
 import React, { useState } from 'react';
 import {
-  Accordion,
+  Accordion ,
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
@@ -14,12 +14,12 @@ const FAQ = () => {
 
   const handleToggle = (value) => {
     setOpenItem(openItem === value ? null : value);
-  };
-
+};
+ 
   return (
     <div>
       <div className='max-w-6xl px-5 md:px-0 mt-9 mx-auto'>
-        <h1 className='text-[#151515] text-[24px] md:text-[33px] xl:text-[40px] font-bold font-serif'>FAQ&apos;s</h1>
+        <h1 className='text-[#151515] text-[24px] md:text-[33px] xl:text-[40px] font-bold '>FAQ&apos;s</h1>
         <div className='flex flex-col md:gap-6 space-y-6 md:flex-row md:justify-between md:items-center'>
           <div className=''>
             <button className='py-2 mt-12 px-6 rounded-md border'>Contact Support</button>
